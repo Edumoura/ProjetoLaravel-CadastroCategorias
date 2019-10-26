5dfd4ddbe753b239a8fa1b5d4e35d734847d676f
Eduardo Moura <oeduardomoura@gmail.com>
2019-10-25 21:27:37 -0300

    Incrementando o index

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,6 +4,11 @@
 	<title>Cadastro de Produtos</title>
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	<link rel="stylesheet"  href="{{ asset('css/app.css') }}">
+	<style type="text/css">
+	    body {
+	    	padding:20px;
+	    }		
+	</style>
 </head>
 <body>
 	<div class="container">
